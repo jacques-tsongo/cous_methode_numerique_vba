636f2b585ab9131806a6f5d9f537ceb09690e52b
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-06-22 20:50:20 +0200

    le calcul de lagrange

--- lagrenge_2/lagrenge_2/Form1.vb
+++ lagrenge_2/lagrenge_2/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Single
+        a = Val(TextBox1.Text)
+        Dim b As Single
+        b = Val(TextBox2.Text)
+        Dim c As Single
+        c = Val(TextBox3.Text)
+
+        TextBox4.Text = 1 / ((a - b) * (a * c))
+        TextBox5.Text = (b * c) / ((a - b) * (a - c))
+        TextBox6.Text = (b * c) / ((a - b) * (a - c))
+    End Sub
 End Class
