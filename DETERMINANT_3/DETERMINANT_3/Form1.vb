82132c8e2100f93fe34a8b8125202b6e010c9dc7
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-06-24 11:50:39 +0200

    initialisation des variables

--- DETERMINANT_3/DETERMINANT_3/Form1.vb
+++ DETERMINANT_3/DETERMINANT_3/Form1.vb
@@ -12,4 +12,29 @@
         TextBox9.Text = ""
         TextBox10.Text = ""
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim a As Single
+        Dim b As Single
+        Dim c As Single
+        Dim d As Single
+        Dim p As Single
+        Dim q As Single
+        Dim r As Single
+        Dim s As Single
+        Dim t As Single
+
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        c = Val(TextBox3.Text)
+        d = Val(TextBox4.Text)
+        p = Val(TextBox5.Text)
+        q = Val(TextBox6.Text)
+        r = Val(TextBox7.Text)
+        s = Val(TextBox8.Text)
+        t = Val(TextBox9.Text)
+
+        TextBox10.Text = ((a * p * t) + (b * q * r) + (c * d * s)) - ((r * p * c) + (s * q * a) + (t * d * b))
+
+    End Sub
 End Class
