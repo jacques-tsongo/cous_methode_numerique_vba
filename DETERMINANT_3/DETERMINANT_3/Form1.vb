de1da03ad77ccbe21bec3fdb0e7b26b1472739ca
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-06-24 11:33:06 +0200

    le code pour le boutton effacer

--- DETERMINANT_3/DETERMINANT_3/Form1.vb
+++ DETERMINANT_3/DETERMINANT_3/Form1.vb
@@ -1,6 +1,15 @@
 ﻿Public Class Form1
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        TextBox10.Text = ""
     End Sub
 End Class
